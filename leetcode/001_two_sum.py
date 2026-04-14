9226186796e3a1800a27a4a4484758f65d19abca
Thato Mofokeng <tsmofokeng1118@gmail.com>
2026-04-14 08:47:29 +0200

    Solve Leetcode #1 - two Sum

--- leetcode/001_two_sum.py
+++ leetcode/001_two_sum.py
@@ -10,9 +10,8 @@
 # to see if they add up to the target.
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-        
